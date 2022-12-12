1217fcd75cc892890efa4366daa9b8776289d73a
badbadblues _ <borovikxv.a@gmail.com>
2022-12-12 20:31:14 +0700

    made modules

---
+++ client/src/main/java/badblues/server/App.java
@@ -0,0 +1,13 @@
+package badblues.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
