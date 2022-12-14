30a0bc36b552571040310e3d4411426653a5eba3
teenspirittt <vladios7@ucloud.com>
2022-12-14 20:33:02 +0700

    working smpt

--- smtp/src/main/java/badblues/smtp/SMTP.java
+++ smtp/src/main/java/badblues/smtp/SMTP.java
@@ -5,21 +5,36 @@ import java.util.Properties;
 import javax.mail.*;
 
 public class SMTP {
-	
+
 	public static void main(String[] args) {
-		
-	    System.out.println("SimpleEmail Start");
-		
-	    String smtpHostServer = "smtp.yandex.ru";
-	    String emailID = "vshabunin25@yandex.ru";
-	    
-	    Properties props = System.getProperties();
+		final String fromEmail = "***"; //requires valid gmail id
+		final String password = "***"; // correct password for gmail id
+		final String toEmail = "***"; // can be any email id
 
-	    props.put("mail.smtp.host", smtpHostServer);
+		System.out.println("SSLEmail Start");
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.yandex.ru"); //SMTP Host
+		props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		//SSL Factory Class
+		props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
+		props.put("mail.smtp.port", "465"); //SMTP Port
 
-	    Session session = Session.getInstance(props, null);
-	    
-	    EmailUtil.sendEmail(session, emailID,"SimpleEmail Testing Subject", "SimpleEmail Testing Body");
+		Authenticator auth = new Authenticator() {
+			//override the getPasswordAuthentication method
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};
+
+
+		MyMessage message = new MyMessage("My First message", "Hello from Africa");
+		try {
+			EmailUtil.send(props,fromEmail,toEmail,message, auth);
+			EmailUtil.sendImageEmail(props,fromEmail,toEmail,message, auth);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
-
 }
